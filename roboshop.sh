0c824c3f1c90d0a885517abfca83aa147a4e7158
KATURI-GOPICHAND <gopichand.cloud11@gmail.com>
2026-03-08 09:54:18 +0530

    actions

--- roboshop.sh
+++ roboshop.sh
@@ -13,7 +13,7 @@ do
     --query 'Instances[0].InstanceId' \
     --output text )
 
-    if [ $instnce == "frontend" ]; then
+    if [ $instance == "frontend" ]; then
         IP=$(
             aws ec2 describe-instances \
             --instance-ids $INSTANCE_ID \
